9aea42fc8c1ff47c256574613dd82434dbc1cc79
Andrés Pedraza Granados <andrespedrazag@outlook.com>
2024-06-18 14:41:57 -0500

    Give up on the camera thing

--- life.c
+++ life.c
@@ -1,14 +1,41 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <math.h>
+#include <stdio.h>
 
 int main(void) {
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(400,300,"Hello World");
+  const int screenWidth = 800;
+  const int screenHeight = 450;
+  const int recWidth = 600;
+  const int recHeight = 400;
+  const int gridSize = 20;
+
+  InitWindow(screenWidth, screenHeight, "Game of Life");
   SetTargetFPS(60);
 
+  Rectangle gameRec = { 25, 25, recWidth, recHeight };
+
+  int currentGesture = GESTURE_NONE;
+  Vector2 touchPosition = { 0, 0 };
+
   while (!WindowShouldClose()) {
+    currentGesture = GetGestureDetected();
+    touchPosition = GetTouchPosition(0);
+
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Hello, world!", 20, 20, 20, BLACK);
+    ClearBackground(DARKBLUE);
+    DrawRectangleLinesEx(gameRec, 2.0f, RAYWHITE);
+    for (int i = 1; i < recWidth/gridSize; i++) {
+      DrawLine(25+gridSize*i, 25, 25+gridSize*i, 25+recHeight, RAYWHITE);
+    }
+    for (int i = 1; i < recHeight/gridSize; i++) {
+      DrawLine(25, 25+gridSize*i, 25+recWidth, 25+gridSize*i, RAYWHITE);
+    }
+    if (CheckCollisionPointRec(touchPosition, gameRec) && currentGesture == GESTURE_TAP) {
+      Vector2 rectanglePos = (Vector2){ 25 + floorf((touchPosition.x - 25)/gridSize)*gridSize, 25 + floorf((touchPosition.y - 25)/gridSize)*gridSize };
+      DrawRectangleV(rectanglePos, (Vector2){ gridSize, gridSize }, RAYWHITE);
+      printf("mouse x: %f, y: %f result x: %f, y: %f\n", touchPosition.x, touchPosition.y, rectanglePos.x, rectanglePos.y);
+    }
     EndDrawing();
   }
 
